e2fb420e7e85285cc3b0a4bd0f87210cc66a849f
viranchiB <viranchibhure69@gmail.com>
2025-04-28 16:34:49 +0530

    Added the JWT and the Custom ContextHolder 1. Custom Context Holder -> Here we use the Thread Local, and it will hold the data until the thread is alive. Also, it will not share the information between the threads.
    2. JWT -> Added the JWT. Request will proceed only when the correct JWT token is provided.

---
+++ API-Gateway/src/main/java/com/linkedin/API_Gateway/Filters/AuthenticationFilter.java
@@ -0,0 +1,67 @@
+package com.linkedin.API_Gateway.Filters;
+
+import com.linkedin.API_Gateway.Service.JWTServiceImpl;
+import io.jsonwebtoken.JwtException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+// Class helps to check if a request contains the header or not
+// If header is present then it will extract the subject/claims(userId) from the header.
+@Component
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+
+    private final Logger log = LoggerFactory.getLogger(AuthenticationFilter.class);
+
+    private final JWTServiceImpl jwtServiceImpl;
+
+    public AuthenticationFilter(JWTServiceImpl jwtServiceImpl) {
+        super(Config.class);
+        this.jwtServiceImpl = jwtServiceImpl;
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            log.info("Received a request for authentication filter");
+
+            // It will extract the Authorization from the request
+            final String tokenHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
+
+            // this will if header is found or not found or Unauthorized
+            if(tokenHeader == null || !tokenHeader.startsWith("Bearer ")) {
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                log.error("Authorization header is missing");
+
+                return exchange.getResponse().setComplete();
+            }
+
+            // If token found
+            final String token = tokenHeader.split("Bearer ")[1];
+
+            try {
+                String userId = this.jwtServiceImpl.getUserIdFromToken(token);
+                ServerWebExchange modifiedExchange = exchange
+                        .mutate()
+                        .request(r -> r.header("X-User-Id", userId))
+                        .build();
+
+                return chain.filter(exchange);
+            }catch (JwtException jwtException){
+                log.error("JWT Exception: {}", jwtException.getLocalizedMessage());
+                log.error(jwtException.getMessage());
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+        };
+    }
+
+
+    public static class Config{
+
+    }
+}
---
+++ Post-Service/src/main/java/com/linkedin/Post_Service/Config/Auth/UserInterceptor.java
@@ -0,0 +1,30 @@
+package com.linkedin.Post_Service.Config.Auth;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+/*
+It will intercept all the incoming request and that requests has a header. So, we can ge the header
+from the request and that header contains the User ID.
+ */
+@Component
+public class UserInterceptor implements HandlerInterceptor {
+
+    // Extracting the User Id from the header and set that user id to contextHolder
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String userID = request.getHeader("X-User-Id");
+        if(userID != null){
+            UserContextHolder.setCurrentUserId(Long.valueOf(userID));
+        }
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    // Clearing the context holder
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        UserContextHolder.clearCurrentUser();
+    }
+}
